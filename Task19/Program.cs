d4016933e0f6f0a44acc1497d866eb73910e80a4
Natali <tasha3425@mail.ru>
2023-03-05 16:45:59 +0300

    Add task 19,21,23

---
+++ Task19/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+int A = InputNumber("Введите пятизначное число: ");
+PrintResult(A);
+
+static int InputNumber(string message)
+{
+    int n = 0;
+    while (true)
+    {
+        try
+        {
+            Console.Write(message);
+            n = (int.Parse(Console.ReadLine() ?? ""));
+            if ((n < 9999) || (n > 99999))
+                Console.WriteLine($"Ошибка ввода данных! Число должно быть пятизначным");
+            else break;
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        }
+    }
+    return n;
+}
+
+static void PrintResult(int A)
+{
+    if ((A / 10000 == A % 10) && ((A / 1000) % 10 == (A % 100) / 10))
+        Console.WriteLine($"Число {A} полиндром");
+    else
+        Console.WriteLine($"Число {A} не полиндром");
+}
---
+++ Task23/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+int A = InputNumber("Введите число: ");
+PrintResult(A);
+
+static int InputNumber(string message)
+{
+    int n = 0;
+    while (true)
+    {
+        try
+        {
+            Console.Write(message);
+            n = (int.Parse(Console.ReadLine() ?? ""));
+            if (n < 1)
+                Console.WriteLine($"Ошибка ввода данных! Число должно быть больше нуля");
+            else break;
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        }
+    }
+    return n;
+}
+
+static void PrintResult(int A)
+{
+    int N = 1;
+    double x;
+    while (N <= A)
+    {
+        x = Math.Pow(N, 3);
+        Console.Write($"{x}, ");
+        N++;
+    }
+}
